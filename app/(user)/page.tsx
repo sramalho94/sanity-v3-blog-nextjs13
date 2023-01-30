25168200efb2fcfd530a32be589a98baaba5fae3
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-30 15:05:10 -0500

    set preview suspense

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+import { client } from '../../lib/sanity.client'
 
-const HomePage = () => {
-  return <div></div>
+const query = groq`
+*[_type=="post"] {
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt desc)`
+
+const HomePage = async () => {
+  if (previewData()) {
+    return <div>Preview Mode</div>
+  }
+
+  const posts = await client.fetch(query)
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
 export default HomePage
