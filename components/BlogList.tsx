8b3513cacf6d3b237553aeab066b79fb06cafe95
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-31 16:45:21 -0500

    changes to bloglist, created ClientSideRoute Component

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import urlFor from '../lib/urlFor'
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid'
 
 type Props = {
   posts: Post[]
@@ -10,7 +11,7 @@ function BlogList({ posts }: Props) {
     <div>
       <hr className="mb-10 border-[#f7ab0a]" />
 
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
           <div key={post._id} className="flex flex-col group cursor-pointer">
             <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
@@ -31,8 +32,29 @@ function BlogList({ posts }: Props) {
                     })}
                   </p>
                 </div>
+
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  {post.categories?.map((category) => (
+                    <div
+                      key={post._id}
+                      className="bg-[#f7ab0a] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                    >
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
+
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="text-gray-500 line-clamp-2">{post.description}</p>
+            </div>
+
+            <p className="flex mt-5 font-bold items-center group-hover:underline">
+              Read Post
+              <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+            </p>
           </div>
         ))}
       </div>
